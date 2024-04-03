5354b94fdf103dcd946a8ef01f925a17fe647aa7
moca xx <mocaxx@mocas-pro.local>
2024-04-03 17:28:30 -0300

    add tiktok media

--- components/Socials.js
+++ components/Socials.js
@@ -1,28 +1,55 @@
 import { Limelight } from "@next/font/google";
 import Link from "next/link";
-import {RiInstagramLine ,RiGithubLine ,  RiLinkedinLine , RiYoutubeLine, RiWhatsappFill} from 'react-icons/ri'
+import {
+  RiInstagramLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiYoutubeLine,
+  RiWhatsappFill,
+  RiTiktokLine,
+} from "react-icons/ri";
 
 const Socials = () => {
   return (
-<div className=" flex items-center gap-x-5 text-lg">
-
-    <Link href={'https://api.whatsapp.com/send/?phone=5511949230302'} className=" hover:text-accent transition-all duration-300">
-      <RiWhatsappFill/>
+    <div className=" flex items-center gap-x-5 text-lg">
+      <Link
+        href={"https://api.whatsapp.com/send/?phone=5511949230302"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiWhatsappFill />
       </Link>
-    <Link href={'https://linkedin.com/in/camismonteiro'} className=" hover:text-accent transition-all duration-300">
-      <RiLinkedinLine/>
+      <Link
+        href={"https://linkedin.com/in/camismonteiro"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiLinkedinLine />
       </Link>
-    <Link href={'https://www.github.com/monteirocamis'} className=" hover:text-accent transition-all duration-300">
-      <RiGithubLine/>
+      <Link
+        href={"https://www.github.com/monteirocamis"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiGithubLine />
       </Link>
-    <Link href={'https://www.youtube.com/@devcamis3d'} className=" hover:text-accent transition-all duration-300">
-      <RiYoutubeLine/>
+      <Link
+        href={"https://www.youtube.com/@devcamis3d"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiYoutubeLine />
       </Link>
-    <Link href={'https://www.instagram.com/devcamis3d'} className=" hover:text-accent transition-all duration-300">
-      <RiInstagramLine/>
+      <Link
+        href={"https://www.instagram.com/camis.3d"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiInstagramLine />
       </Link>
-</div>
-  ) 
+      <Link
+        href={"https://www.tiktok.com/camis3d"}
+        className=" hover:text-accent transition-all duration-300"
+      >
+        <RiTiktokLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
